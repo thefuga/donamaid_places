aa56cbe77c9175836d0a2292610e9c9489ad70b9
Erick C. Fuga <erick_fuga@hotmail.com>
2018-07-13 17:35:18 -0300

    Updated with new classes.

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ import Places
 
 if __name__ == "__main__":
 
-    places = Places("AIzaSyBgZPQc5CswWQD63YSIiXO_AcS-g4wp-n4")    
-    cities = ("Londrina, PR", "Maringa, PR", "Caxias, RS", "Pelotas, RS", "Rio Grande, RS", "Santa Maria, RS")
+    gplaces = Places.Places("AIzaSyBgZPQc5CswWQD63YSIiXO_AcS-g4wp-n4")
+    #cities = ("Londrina, PR", "Maringa, PR", "Caxias, RS", "Pelotas, RS", "Rio Grande, RS", "Santa Maria, RS")
+    cities = ("Pelotas", )
 
-    places = ("Escritórios de contabilidade", "Escritórios administrativos", "Escritórios de arquitetura",
-    "Escritórios de advogacia", "clinicas odontológicas", "Clinicas psicológicas", "Coworking",
-    "Agências de publicidade", "Academias", "Pet shop")
+    #places = ("Escritórios de contabilidade", "Escritórios administrativos", "Escritórios de arquitetura",
+    #"Escritórios de advogacia", "clinicas odontológicas", "Clinicas psicológicas", "Coworking",
+    #"Agências de publicidade", "Academias", "Pet shop")
+    places = ("Academias", )
 
+    places_ids = []
     for city in cities:
-        for place in places:
-            create_dataset.generate_dataset(seek_places.get_place_info(key, seek_places.get_places(key, place, location=city, radius=20000)), place + " em " + city)
+        vicinities = gplaces.get_vicinities(gplaces.get_pinpoints(city, radius=2000), city=city)
+
+        for vicinity in vicinities:
+            for place in places:
+                places_ids.append(gplaces.get_places(place + ", " + vicinity, location=city, radius=2000)) #get the places ids. Also get the ids without specifiying a vicinity to compare.
+    print(len(places_ids))
